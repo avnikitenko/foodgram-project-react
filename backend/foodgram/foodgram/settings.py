b8015dc21f8e434861a3e252b13af6d455471ef3
Никитенко Алексей <ya.avnikitenko@gmail.com>
2022-06-28 20:57:45 +0300

    some fixes

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'key')
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
